e7583c9b449d921c0b9e251529cd46bd081e034e
will0684 <jordan.walcaraz@gmail.com>
2019-07-08 15:01:14 -0400

    Added endpoint for updating a location document

--- src/routes/locations.routes.js
+++ src/routes/locations.routes.js
@@ -3,13 +3,37 @@ import express from 'express';
 
 let router = express.Router();
 
-// Get Locations
+// Get All Locations
 router.get('/locations', (req, res) => {
   LocationsModel.find((err, locations) => {
     if (err) {
       res.sendStatus(500).send({ error: 'Internal Server Error' });
     } else {
-      res.json(locations);
+      res.status(200).send(locations);
+    }
+  });
+});
+
+// Get One Location
+router.get('/locations/:id', (req, res) => {
+  const locationId = req.params.id;
+  LocationsModel.findById(locationId, (err, locationDoc) => {
+    if (err) {
+      res.sendStatus(403).send({ error: 'Could not find the location' });
+    } else {
+      res.status(200).send(locationDoc);
+    }
+  });
+});
+
+// Update a Location
+router.put('/locations/:id', (req, res) => {
+  const locationId = req.params.id;
+  LocationsModel.findByIdAndUpdate(locationId, (err, updatedLocationDoc) => {
+    if (err) {
+      res.sendStatus(403).send({ error: 'Could not update location' });
+    } else {
+      res.statusCode(200).send(updatedLocationDoc);
     }
   });
 });
