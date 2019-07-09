48b898ebc27a0687646cc304371886ef4c6911d2
kevinNghiem <kevin.nghiem@hrsdc-rhdcc.gc.ca>
2019-07-09 14:53:31 -0400

    get and post routes added to the apppointments route

--- src/routes/appointments.routes.js
+++ src/routes/appointments.routes.js
@@ -3,4 +3,21 @@ import express from 'express';
 
 let router = express.Router();
 
+// GET
+router.get('/appointments', (req, res) => {
+  if (!req.query.appointmentId) {
+    return res.status(400).send('Missing URL parameter: appointmentId');
+  }
+
+  AppointmentsModel.findOne({
+    appointmentId: req.query.appointmentId
+  })
+    .then(doc => {
+      res.json(doc);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
 export default router;
