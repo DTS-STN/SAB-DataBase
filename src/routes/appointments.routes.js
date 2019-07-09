a1a7814ce3263460a88c2e149c3ec3b6b1e64f34
kevinNghiem <kevin.nghiem@hrsdc-rhdcc.gc.ca>
2019-07-09 15:01:59 -0400

    get and post routes added to the apppointments route updated

--- src/routes/appointments.routes.js
+++ src/routes/appointments.routes.js
@@ -20,4 +20,36 @@ router.get('/appointments', (req, res) => {
     });
 });
 
+//post
+
+router.post('/appointments', (req, res) => {
+  if (!req.body) {
+    return res.status(400).send('Request body is missing');
+  }
+
+  if (!req.body.appointmentId) {
+  }
+  //json format to test on postman and issomonia
+  // {
+  //     "locationId" : "hello1234",
+  //         "locationAddress" : "jotaru@kujo.com",
+  //             "cic" : "ab123456789",
+  //                 "date" : "2019-09-21",
+  // }
+
+  let model = new AppointmentsModel(req.body);
+  model
+    .save()
+    .then(doc => {
+      if (!doc || doc.length === 0) {
+        return res.status(500).send(doc);
+      }
+
+      res.status(201).send(doc);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
 export default router;
