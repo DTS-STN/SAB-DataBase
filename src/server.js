29da879fe61727f9a7f4b1d4730c4a1682f092b4
MarcoGoC <marco.flores@hrsdc-rhdcc.gc.ca>
2019-07-29 13:02:15 -0400

    Task 898 End Points to get the info for drop downs
    Signed-off-by: MarcoGoC <marco.flores@hrsdc-rhdcc.gc.ca>

--- src/server.js
+++ src/server.js
@@ -12,7 +12,8 @@ import bodyParser from 'body-parser';
 require('dotenv').config();
 
 let app = express();
-mongoose.connect(process.env.CONNECTION_STRING);
+mongoose.connect(process.env.CONNECTION_STRING, { useNewUrlParser: true });
+mongoose.set('useCreateIndex', true);
 
 mongoose.connection
   .once('open', () => {
