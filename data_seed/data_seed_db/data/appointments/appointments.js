d25f421697f2288d7d33f6d23abe3aa1bca1e89f
will0684 <jordan.walcaraz@gmail.com>
2019-08-01 08:55:23 -0400

    Created document objects

---
+++ data_seed/data_seed_db/data/appointments/appointments.js
@@ -0,0 +1,52 @@
+const { getObjectId } = require('../../helpers/index');
+const { moment } = require('moment');
+
+const appointments = [
+  {
+    id: getObjectId('abc123'),
+    appointmentId: 'abc123',
+    locationId: 'SC-0001',
+    bioKitId: 'asd123',
+    bil: 'abc123',
+    date: moment()
+      .startOf('week')
+      .add(14, 'days')
+      .toDate(),
+    timeSlot: '8:00',
+    dateSubmitted: moment().toDate(),
+    cancelledByClient: false,
+    cancelledByLocation: false
+  },
+  {
+    id: getObjectId('huy668'),
+    appointmentId: 'huy668',
+    locationId: 'SC-0001',
+    bioKitId: 'asd123',
+    bil: 'ggg666',
+    date: moment()
+      .startOf('week')
+      .add(14, 'days')
+      .toDate(),
+    timeSlot: '8:15',
+    dateSubmitted: moment().toDate(),
+    cancelledByClient: false,
+    cancelledByLocation: false
+  },
+  {
+    id: getObjectId('ret688'),
+    appointmentId: 'ret688',
+    locationId: 'SC-0001',
+    bioKitId: 'asd123',
+    bil: 'gye429',
+    date: moment()
+      .startOf('week')
+      .add(14, 'days')
+      .toDate(),
+    timeSlot: '8:30',
+    dateSubmitted: moment().toDate(),
+    cancelledByClient: false,
+    cancelledByLocation: false
+  }
+];
+
+module.exports = appointments;
