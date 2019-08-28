ff904037ae9963d501c921fb0eb63efa2b129844
AA <andr0272@algonquinlive.com>
2019-08-28 16:03:08 -0400

    Added record counter

--- Tools/LocationImporter.js
+++ Tools/LocationImporter.js
@@ -1,14 +1,18 @@
 // Temporary tool used to import location data to our mongo data model 
 // from a CSV file provided by product owners
 
-const csv = require('csv-parser')
-const fs = require('fs')
+import csv from 'csv-parser';
+import { createReadStream } from 'fs';
 
 const sites = []
+var counter = 0
+var readstream = createReadStream(require('path').resolve('Tools/Biometrics_Sitescsv.csv'))
 
-fs.createReadStream(require('path').resolve(__dirname, 'Biometrics_Sitescsv.csv'))
-    .pipe(csv())
-    .on('data', (data) => {sites.push(data)})
+readstream.pipe(csv())
+    .on('data', (data) => {
+        sites.push(data)
+        counter++
+    })
     .on('end', () => {
-        console.log(sites)
+        console.log(`${counter} records read from file`)
     })
