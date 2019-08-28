d04bd55b4366f1f9dbd224bae5793835533461d6
will0684 <jordan.walcaraz@gmail.com>
2019-08-28 14:56:32 -0400

    Fixed linted problems

--- Tools/LocationImporter.js
+++ Tools/LocationImporter.js
@@ -1,14 +1,18 @@
-// Temporary tool used to import location data to our mongo data model 
+// Temporary tool used to import location data to our mongo data model
 // from a CSV file provided by product owners
 
-const csv = require('csv-parser')
-const fs = require('fs')
+const csv = require('csv-parser');
+const fs = require('fs');
 
-const sites = []
+const sites = [];
 
-fs.createReadStream(require('path').resolve(__dirname, 'Biometrics_Sitescsv.csv'))
-    .pipe(csv())
-    .on('data', (data) => {sites.push(data)})
-    .on('end', () => {
-        console.log(sites)
-    })
+fs.createReadStream(
+  require('path').resolve(__dirname, 'Biometrics_Sitescsv.csv')
+)
+  .pipe(csv())
+  .on('data', data => {
+    sites.push(data);
+  })
+  .on('end', () => {
+    console.log(sites);
+  });
