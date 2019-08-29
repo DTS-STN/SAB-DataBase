136e3e4a060c187ecfff0e6414cb040bda6a7068
AA <andr0272@algonquinlive.com>
2019-08-29 09:27:43 -0400

    skeleton functions // sudo code

--- Tools/DatabaseHelper.js
+++ Tools/DatabaseHelper.js
@@ -1,6 +1,12 @@
 // Mongo authentication and insertion
 // Import mongo model
+import locationModel from "../src/models/location.model";
+// Initialise connection to database 
+export const init = () => {
+// create connection string through env variables
+}
 
-// Function initialise database
-
-// Function insert (mongo model)
+// Takes one complete model, inserts it into the database
+export const insert = (locationModel) => {
+// send this documentr to the database
+}
--- Tools/LocationImporter.js
+++ Tools/LocationImporter.js
@@ -16,3 +16,21 @@ readstream.pipe(csv())
     .on('end', () => {
         console.log(`${counter} records read from file`)
     })
+
+// Main
+// db.init() Init database
+// readFromFile(path)
+// for x in sites{
+//  db.insert(x)
+//}
+
+// Function get read stream, seperate lines to sites[]
+const readFromFile = (path) => {
+// Create read stream from file at {path}
+// parse each line into a mongo model
+}
+
+// seperate the location string (undecided / unused)
+const seperateLocation = (addressString) => {
+
+}
