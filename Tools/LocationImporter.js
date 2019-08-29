b963717e39583993a4ac3e39ac1bc294b7629493
will0684 <jordan.walcaraz@gmail.com>
2019-08-29 13:52:15 -0400

    Added bioKitAmount to models created from csv data

--- Tools/LocationImporter.js
+++ Tools/LocationImporter.js
@@ -44,7 +44,8 @@ const createModels = location => {
     locationCity: addressArray[addressArray.length - 3],
     postalCode: addressArray[addressArray.length - 1],
     locationProvince: addressArray[addressArray.length - 2],
-    locationProvinceFr: addressArray[addressArray.length - 2]
+    locationProvinceFr: addressArray[addressArray.length - 2],
+    bioKitAmount: parseInt(location.bioKits)
   });
   console.log(model);
   locationsFormatted.push(model);
