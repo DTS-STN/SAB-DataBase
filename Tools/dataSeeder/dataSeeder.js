a885f37cd4e333eab76fcd6141c43b3b78c52f78
will0684 <jordan.walcaraz@gmail.com>
2019-09-09 09:20:55 -0400

    Formatting

--- Tools/dataSeeder/dataSeeder.js
+++ Tools/dataSeeder/dataSeeder.js
@@ -1,96 +1,64 @@
 import LocationModel from '../../src/models/location.model';
 import AppointmentModel from '../../src/models/appointments.model';
 import BiokitModel from '../../src/models/biokits.model';
-import mongoose from 'mongoose';
-require('dotenv').config();
+import * as db from '../DatabaseHelper';
+import * as Randomizers from './Randomizers';
 
-// get input for number of locations and appointments
-// start a loop for each model
-// populate with random data
-// add each to database
-let numAppoints = process.env.NUMAPPOINTMENTS || 100
-let numLocations = process.env.NUMLOCATIONS || 100
+let numAppoints = process.env.NUMAPPOINTMENTS || 100;
+let numLocations = process.env.NUMLOCATIONS || 100;
 
-function randomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+const populateDatabase = async () => {
+  for (let i = 0; i < numAppoints; i++) {
+    await AppointmentModel.create({
+      appointmentId: i,
+      clientEmail: `${Randomizers.randomString(10)}@example.com`,
+      phoneNumber: Randomizers.randomInt(10000000000, 99999999999),
+      locationId: Randomizers.randomString(3),
+      bioKitId: Randomizers.randomString(6),
+      bil: Randomizers.randomString(3),
+      date: Randomizers.randomDate(),
+      dateSubmitted: Randomizers.randomDate(),
+      maintenance: i % 2,
+      cancelledByClient: i % 2,
+      cancelledByLocation: i % 2
+    });
+  }
 
-function randomDate() {
-    // aprox nr of days since 1970 untill 2000: 30years * 365 days
-    var nr_days1 = 30*365;
-    // aprox nr of days since 1950 untill 1970: 20years * 365 days
-    var nr_days2 = -20*365;
-
-    // milliseconds in one day
-    var one_day=1000*60*60*24
-
-    // get a random number of days passed between 1950 and 2000
-    var days = randomInt(nr_days2, nr_days1);
-
-    return new Date(days*one_day)
-}
-
-function randomString(length) {
-    var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz";
-      var result = '';
-      for (var i=0; i<length; i++) {
-          var rnum = Math.floor(Math.random() * chars.length);
-          result += chars.substring(rnum,rnum+1);
-      }
-      return result;
-    }
-// open database connection
-mongoose.connect('mongodb://localhost:27018/test', {useNewUrlParser: true});
-const db = mongoose.connection
-
-db.on('error', console.error.bind(console, 'connection error:'))
-db.once('open', function() {
-    console.log('Connection to the database established');
-});
-
-for( let i = 0; i < numAppoints; i++){
-    let appointment = AppointmentModel.create({
-        appointmentId: i,
-        clientEmail: `${randomString(10)}@example.com`,
-        phoneNumber: randomInt(10000000000, 99999999999),
-        locationId: randomString(3),
-        bioKitId: randomString(6),
-        bil: randomString(3),
-        date: randomDate(),
-        dateSubmitted: randomDate(),
-        maintenance: i%2,
-        cancelledByClient: i%2,
-        cancelledByLocation: i%2
+  for (let i = 0; i < numLocations; i++) {
+    await LocationModel.create({
+      locationId: i,
+      locationName: Randomizers.randomString(8),
+      locationRegion: Randomizers.randomString(3),
+      locationType: Randomizers.randomString(3),
+      locationAddress: Randomizers.randomString(15),
+      locationCity: Randomizers.randomString(6),
+      postalCode: Randomizers.randomString(6),
+      locationProvince: Randomizers.randomString(2),
+      hours: `${Randomizers.randomInt(8, 10)}:00-${Randomizers.randomInt(
+        15,
+        17
+      )}:00`,
+      closures: [
+        {
+          periodStart: Randomizers.randomDate(),
+          periodEnd: Randomizers.randomDate()
+        }
+      ],
+      bioKitAmount: Randomizers.randomInt(1, 999),
+      bioKits: new BiokitModel({
+        bioKitId: Randomizers.randomString(5),
+        accessible: i % 2,
+        available: i % 2
       })
+    });
+  }
+};
+
+async function main() {
+  db.init().then(async () => {
+    await populateDatabase();
+    db.close();
+  });
 }
 
-for( let i = 0; i < numLocations; i++){
-    let location = LocationModel.create({
-        locationId: i,
-        locationName: randomString(8),
-        locationRegion: randomString(3),
-        locationType: randomString(3),
-        locationAddress: randomString(15),
-        locationCity: randomString(6),
-        postalCode: randomString(6),
-        locationProvince: randomString(2),
-        hours: `${randomInt(0,24)}:${randomInt(0,24)}-${randomInt(0,24)}:${randomInt(0,24)}`,
-        closures: [
-          {
-            periodStart: randomDate(),
-            periodEnd: randomDate()
-          }
-        ],
-        bioKitAmount: randomInt(1,999),
-        bioKits: BiokitModel.create({
-            bioKitId: randomString(5),
-            accessible: i%2,
-            available: i%2
-        })
-      })
-}
-
-// db.close()
-db.on('disconnected', () => {
-    console.log('Connection closed');
-})
+main();
