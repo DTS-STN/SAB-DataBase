53ac30b14c88ce49c530460b21b0c416e723e59b
AA <adam.andrews2797@gmail.com>
2019-10-04 10:42:43 -0400

    Adding appointments route

--- src/routes/appointments.routes.js
+++ src/routes/appointments.routes.js
@@ -1,31 +1,54 @@
 import AppointmentsModel from '../models/appointments.model';
+// eslint-disable-next-line no-unused-vars
 import moment from 'moment';
 import express from 'express';
+import locationModel from '../models/location.model';
 
 let router = express.Router();
 
-// TODO: Create endpoint for cancelling appointments
+function respondToFind(res, err, errMsg, object) {
+  if (err) {
+    res.status(errMsg.code).send({ error: err, message: errMsg.msg });
+  } else {
+    res
+      .status(200)
+      .type('application/json')
+      .send(object);
+  }
+}
+
+const couldNotGetAppointments = {
+  code: 403,
+  msg: 'Could not get appointments'
+};
+
+// const couldNotGetTimeslots = {
+//   code: 403,
+//   msg: 'Could not get timeslots'
+// };
 
 // GET appointments for a given location for a given day
 // or month. Days and months are passed as query strings
 router.get('/appointments/:locationId', (req, res) => {
   let day = req.query.day;
   let month = req.query.month;
+  let fromDate = req.query.from;
+  let to = req.query.to;
+
   if (day) {
-    day = moment(day, 'DD-MM-YYYY');
-    AppointmentsModel.find({
-      locationId: req.params.locationId,
-      date: {
-        $gte: day.startOf('day').toDate(),
-        $lte: day.endOf('day').toDate()
+    day = moment(day, 'YYYY-MM-DD');
+    AppointmentsModel.find(
+      {
+        locationId: req.params.locationId,
+        date: {
+          $gte: day.startOf('day').toDate(),
+          $lte: day.endOf('day').toDate()
+        }
+      },
+      (err, appointmentsDocs) => {
+        respondToFind(res, err, couldNotGetAppointments, appointmentsDocs);
       }
-    })
-      .then(docs => {
-        res.json(docs);
-      })
-      .catch(err => {
-        res.status(500).json(err);
-      });
+    );
   } else if (month) {
     let startDate = moment()
       .month(month)
@@ -35,36 +58,106 @@ router.get('/appointments/:locationId', (req, res) => {
       .month(month)
       .endOf('month')
       .toDate();
-    AppointmentsModel.find({
-      locationId: req.params.locationId,
-      date: {
-        $gte: startDate,
-        $lte: endDate
+    AppointmentsModel.find(
+      {
+        locationId: req.params.locationId,
+        date: {
+          $gte: startDate,
+          $lte: endDate
+        },
+        cancelledByClient: false,
+        cancelledByLocation: false
       },
-      cancelledByClient: false,
-      cancelledByLocation: false
-    })
-      .then(doc => {
-        res.json(doc);
-      })
-      .catch(err => {
-        res.status(500).json(err);
-      });
+      (err, appointmentsDocs) => {
+        respondToFind(res, err, couldNotGetAppointments, appointmentsDocs);
+      }
+    );
+  } else if (fromDate) {
+    fromDate = moment(fromDate, 'DD-MM-YYYY');
+    to = moment(to, 'DD-MM-YYYY');
+    AppointmentsModel.find(
+      {
+        locationId: req.params.locationId,
+        date: {
+          $gte: fromDate.startOf('day').toDate(),
+          $lte: to.endOf('day').toDate()
+        }
+      },
+      (err, appointmentsDocs) => {
+        respondToFind(res, err, couldNotGetAppointments, appointmentsDocs);
+      }
+    );
   } else {
     let now = moment().toDate();
-    AppointmentsModel.find({
-      locationId: req.params.locationId,
-      date: {
-        $gte: now
+    AppointmentsModel.find(
+      {
+        locationId: req.params.locationId,
+        date: {
+          $gte: now
+        }
+      },
+      (err, appointmentsDocs) => {
+        respondToFind(res, err, couldNotGetAppointments, appointmentsDocs);
       }
-    })
-      .then(doc => {
-        res.json(doc);
-      })
-      .catch(err => {
-        res.status(500).json(err);
-      });
+    );
   }
+});
+
+// GET available timeslots for a location on a given day
+router.get('/appointments/timeslots/:locationId', (req, res) => {
+  let day = req.query.day;
+  if (!day) {
+    res.status(500).json('Day parameter is missing');
+  }
+  locationModel
+    .findOne({
+      locationId: req.params.locationId
+    })
+    .then(loc => {
+      if (!loc) {
+        res.status(403).json('Error: Location does not exist');
+      }
+      day = moment(day, 'YYYY-MM-DD').utc();
+      const bioKitCount = loc.bioKitCount;
+      let hours = loc.hours.split('-');
+      let start = hours[0];
+      let end = hours[1];
+      let timeSlots = getTimeStops(start, end);
+
+      AppointmentsModel.find({
+        locationId: req.params.locationId,
+        date: {
+          $gte: day.startOf('day').toDate(),
+          $lte: day.endOf('day').toDate()
+        }
+      })
+        .then(appointments => {
+          let appointmentCounts = appointments
+            .map(a =>
+              moment(a.date)
+                .utc()
+                .format('hh:mm a')
+            )
+            .reduce((acc, curr) => {
+              acc[`${curr}`] = (acc[`${curr}`] || 0) + 1;
+              return acc;
+            }, {});
+          const fullTimeSlots = Object.keys(appointmentCounts).filter(
+            ac => appointmentCounts[`${ac}`] >= bioKitCount
+          );
+          //return all the timeslots that aren't in the list of fullAppointments
+          return timeSlots.filter(ts => !fullTimeSlots.includes(ts.value));
+        })
+        .then(timeSlots => {
+          res.status(200).json(timeSlots);
+        })
+        .catch(err => {
+          res.status(500).json(`Error:` + err.message);
+        });
+    })
+    .catch(err => {
+      res.status(500).json(`Could not retrieve timeslots: ${err}`);
+    });
 });
 
 // GET a current (not in the past) appointment with it's BIL field
@@ -90,7 +183,11 @@ router.post('/appointments/temp', (req, res) => {
     return res.status(400).send('Request body is missing');
   }
   let model = new AppointmentsModel(req.body);
-
+  model.confirmation = null;
+  model.maintenance = false;
+  model.cancelledByClient = false;
+  model.cancelledByLocation = false;
+  model.dateConfirmed = null;
   model.expires = moment()
     .add(5, 'minutes')
     .toDate();
@@ -112,8 +209,9 @@ router.put('/appointments/confirm/:documentId', (req, res) => {
   // Get temporary appointment document
   AppointmentsModel.findById(req.params.documentId).then(doc => {
     // Remove expiry and change to confirmed appointment
+    console.log(doc.dateConfirmed);
     if (doc.dateConfirmed !== null) {
-      return res.status(400).send('Appointment has already been confirmed');
+      return res.status(400).send(doc);
     }
     doc.confirmation = hashFromData(doc.clientEmail, doc.bil);
     doc.expires = null;
@@ -144,6 +242,28 @@ const hashFromData = (email, paperFileNumber) => {
     hash |= 0;
   }
   return hash;
+};
+
+const getTimeStops = (start, end) => {
+  var startTime = moment(start, 'hh:mm');
+  var endTime = moment(end, 'hh:mm');
+
+  if (endTime.isBefore(startTime)) {
+    endTime.add(1, 'day');
+  }
+
+  var timeStops = [];
+
+  while (startTime <= endTime) {
+    const timeStop = moment(startTime).format('hh:mm a');
+    timeStops.push({
+      value: timeStop,
+      name: timeStop
+    });
+    startTime.add(15, 'minutes');
+  }
+
+  return timeStops;
 };
 
 export default router;
